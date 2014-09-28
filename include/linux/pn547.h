63dec9e3fdb36576fa7954008e42bb13ac6c9878
Chirayu Desai <cdesai@cyanogenmod.org>
2014-09-28 18:20:21 +0530

    misc: Update pn547 NFC Controller driver
    * Kanged from Samsung N005XXUENA6
      CyanogenMod/android_kernel_samsung_hlte
      (464aaea7b04697241fd7fa7b69bf8debe6843071)
    * Adapted for Sony.
    
    Change-Id: I8049fdacb06990d73efd552443ac4b282c3541fc
    
    Conflicts:
    	drivers/misc/Kconfig
    	drivers/misc/pn547.c
    	include/linux/nfc/pn547.h

--- include/linux/pn547.h
+++ include/linux/pn547.h
@@ -1,8 +1,5 @@
 /*
- *
- *  Integrator architecture timex specifications
- *
- *  Copyright (C) 1999 ARM Limited
+ * Copyright (C) 2010 Trusted Logic S.A.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
