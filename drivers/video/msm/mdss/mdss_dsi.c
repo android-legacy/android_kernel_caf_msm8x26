e28522936889f69da0acce1dbc9414fdeecf0fc1
Angelo G. Del Regno -- EternityProject Team Manager <kholk11@gmail.com>
2014-12-22 20:34:47 +0000

    video: msm: mdss_dsi: Send display on command prior unblanking
    Obviously, if the display is off, unblanking isn't going to happen.
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Change-Id: Id1eb99a0f65b830c9690808773d69ca6917f04fd

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1040,13 +1040,13 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		break;
 	case MDSS_EVENT_PANEL_ON:
 		ctrl_pdata->ctrl_state |= CTRL_STATE_MDP_ACTIVE;
-		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
-			rc = mdss_dsi_unblank(pdata);
 #ifdef CONFIG_FB_MSM_MDSS_PANEL_SPECIFIC
 		if (ctrl_pdata->spec_pdata->disp_on_in_hs
 			&& ctrl_pdata->spec_pdata->disp_on)
 			rc = ctrl_pdata->spec_pdata->disp_on(pdata);
 #endif
+		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
+			rc = mdss_dsi_unblank(pdata);
 		break;
 	case MDSS_EVENT_BLANK:
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
