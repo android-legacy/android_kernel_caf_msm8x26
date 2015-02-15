4ca617fde283e7ce65817fc22c4d6322800da658
erikcas <erikcas1972@gmail.com>
2015-02-15 19:29:33 +0100

    omnify
    Change-Id: I102d98651bf639e9e6c7f2772b09571d6ec2d098

--- Android.mk
+++ Android.mk
@@ -16,11 +16,11 @@
 # Android makefile to build kernel as a part of Android Build
 
 ifneq ($(BUILD_KERNEL),)
-ifeq ($(filter-out amami aries castor eagle flamingo honami leo scorpion seagull sirius tianchi,$(TARGET_DEVICE)),)
+ifeq ($(filter-out amami aries castor eagle eagle_lte flamingo honami leo scorpion seagull sirius tianchi,$(TARGET_DEVICE)),)
 
 KERNEL_SRC := $(call my-dir)
 # kernel configuration - mandatory:
-TARGET_KERNEL_CONFIG ?= $(notdir $(wildcard $(KERNEL_SRC)/arch/arm/configs/aosp_*_$(TARGET_DEVICE)_defconfig))
+TARGET_KERNEL_CONFIG ?= $(notdir $(wildcard $(KERNEL_SRC)/arch/arm/configs/omni_*_$(TARGET_DEVICE)_defconfig))
 KERNEL_DEFCONFIG := $(TARGET_KERNEL_CONFIG)
 
 ifeq ($(OUT_DIR),out)
